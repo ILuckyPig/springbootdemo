70b34b61be113b2bb0868a997dcb150e5dacbce5
ILuckyPig <980534016.qq.com>
2019-08-11 14:53:09 +0800

    @Aspect AOP日志处理

--- src/main/java/com/lu/controller/HelloWorldController.java
+++ src/main/java/com/lu/controller/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.lu.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/hello")
-    public String hello() throws Exception {
+    @RequestMapping("/myerror")
+    public String error() throws Exception {
         throw new Exception("发生错误");
+    }
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
     }
 }
